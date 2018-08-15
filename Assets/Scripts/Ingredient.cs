3dd80a9423157e1cb31fbf6507dc2181aa496aea
gaojinjin <>
2018-08-15 23:03:43 +0800

    添加Editor编辑器，调节脚本在Inspector面板中显示内容。

---
+++ Assets/Scripts/Ingredient.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public enum IngredientUnit { Spoon, Cup, Bowl, Piece }
+
+// Custom serializable class
+[System.Serializable]
+public class Ingredient
+{
+    public string name;
+    public int amount = 1;
+    public IngredientUnit unit;
+}
+
+//This is not an editor script
+class IngredientTesting : MonoBehaviour
+{
+    [Range(1, 10)]
+    public int myInt;
+    public Ingredient potionResult;
+    public Ingredient[] potionIngredients;
+
+    void Update()
+    {
+        // Update logic here...
+    }
+}
